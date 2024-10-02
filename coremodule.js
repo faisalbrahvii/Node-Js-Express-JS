ac2d0a8ccb943962104c812e711e8912d3704e48
~Faysal <114810964+faisalbrahvii@users.noreply.github.com>
2024-10-03 02:46:00 +0500

    Add files via upload

---
+++ coremodule.js
@@ -0,0 +1,6 @@
+const myFile = require('fs').writeFileSync;
+console.log("Node JS Core Modules");
+
+
+// myFile.writeFileSync("coremodules.txt", "Node JS Core Modules ")
+ myFile("coremodules.txt", "Node JS Core Modules ");
